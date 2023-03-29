19dca3a20a869e4667dd932c2dd945d4870b200e
GnomZoltan <stastopilnytskii@gmail.com>
2023-03-29 10:30:00 +0300

    Template declared (first step)

--- LabWorkTemplate/main.cpp
+++ LabWorkTemplate/main.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include "points.h"
+#include <iomanip>
+
+CPoint<int> read_point();
+
+int main()
+{
+	CPoint<int> A = read_point();
+	CPoint<int> B = read_point();
+	CPoint<int> C = read_point();
+
+	std::cout << " A = " << A << '\n' << " B = " << B << '\n' << " C = " << C << '\n';
+	//int P = A.distance(B) + B.distance(C) + C.distance(A);
+	//double p = P * 0.5;
+	int a = A.distance(B);
+	int b = B.distance(C);
+	int c = C.distance(A);
+	int P = a + b + c;
+	double p = P * 0.5;
+	double S = sqrt(p * (p - a) * (p - b) * (p - c));
+	std::cout << "P  = " << P << " S = " << S << '\n';
+	return 0;
+}
+
+CPoint<int> read_point()
+{
+	std::cout << "Input 2 integers: ";
+	int x, y;
+	std::cin >> x >> y;
+	return CPoint<int>(x, y);
+}
---
+++ LabWorkTemplate/points.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+template<typename TNum>
+class CPoint {
+private:
+    TNum x;
+    TNum y;
+public:
+    CPoint(TNum a, TNum b) : x(a), y(b) {}
+    CPoint() : x(0), y(0) {}
+
+    TNum get_x() const {
+        return x;
+    }
+
+    TNum get_y() const {
+        return y;
+    }
+
+    void print_on(std::ostream& os) const {
+        os << '(' << x << "; " << y << ')';
+    }
+
+    TNum distance(const CPoint& p) const;
+    CPoint operator+(const CPoint& p) const {
+        return CPoint(this->x + p.x, this->y + p.y);
+    }
+
+    CPoint operator-(const CPoint& p) const {
+        return CPoint(this->x - p.x, this->y - p.y);
+    }
+};
+
+template<typename TNum>
+inline TNum CPoint<TNum>::distance(const CPoint& p) const
+{
+    return sqrt(pow(x - p.x, 2) + pow(y - y.x, 2));
+}
+
+template<typename TNum>
+std::ostream& operator << (std::ostream& os, const CPoint<TNum>& A) {
+    A.print_on(os);
+    return os;
+}
+
+template<typename TNum>
+CPoint<TNum> operator*(const CPoint <TNum>& p, TNum n) {
+    return CPoint<TNum>(p.get_x() * n, p.get_y() * n);
+}
