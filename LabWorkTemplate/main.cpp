1351cef8f23f1a988c542fef59e180cfa07b18c5
GnomZoltan <stastopilnytskii@gmail.com>
2023-03-29 10:42:22 +0300

    Template declared (second step)

--- LabWorkTemplate/main.cpp
+++ LabWorkTemplate/main.cpp
@@ -1,32 +1,38 @@
-#include <iostream>
-#include "points.h"
+﻿#include <iostream>
+#include "Points.h"
 #include <iomanip>
 
 CPoint<int> read_point();
 
 int main()
 {
-	CPoint<int> A = read_point();
-	CPoint<int> B = read_point();
-	CPoint<int> C = read_point();
-
-	std::cout << " A = " << A << '\n' << " B = " << B << '\n' << " C = " << C << '\n';
-	//int P = A.distance(B) + B.distance(C) + C.distance(A);
-	//double p = P * 0.5;
-	int a = A.distance(B);
-	int b = B.distance(C);
-	int c = C.distance(A);
-	int P = a + b + c;
-	double p = P * 0.5;
-	double S = sqrt(p * (p - a) * (p - b) * (p - c));
-	std::cout << "P  = " << P << " S = " << S << '\n';
-	return 0;
+    /* Введіть з клавіатури точки А, В, С (координати задані цілими числами),
+     * що є вершинами деякого трикутника. Обчисліть і надрукуйте
+     * периметр і площу цього трикутника */
+    CPoint<int> A = read_point();
+    CPoint<int> B = read_point();
+    CPoint<int> C = read_point();
+    std::cout << " A = " << A << '\n' << " B = " << B << '\n' << " C = " << C << '\n';
+    double a = A.distance(B);
+    double b = B.distance(C);
+    double c = C.distance(A);
+    double P = a + b + c;
+    double p = P * 0.5;
+    double S = sqrt(p * (p - a) * (p - b) * (p - c));
+    std::cout << " P = " << P << "    S = " << S << '\n';
+    /* Чи є серед заданих точок «особливі» – такі,
+     * що лежить в ε-околі початку координат? */
+    std::cout << "Input the eps: ";
+    double eps; std::cin >> eps;
+    if (A.distance(CPoint<int>()) <= eps) std::cout << A << " is special\n";
+    if (B.distance(CPoint<int>()) <= eps) std::cout << B << " is special\n";
+    if (C.distance(CPoint<int>()) <= eps) std::cout << C << " is special\n";
 }
 
 CPoint<int> read_point()
 {
-	std::cout << "Input 2 integers: ";
-	int x, y;
-	std::cin >> x >> y;
-	return CPoint<int>(x, y);
+    std::cout << "Input two integers: ";
+    int x, y;
+    std::cin >> x >> y;
+    return CPoint<int>(x, y);
 }
--- LabWorkTemplate/points.h
+++ LabWorkTemplate/points.h
@@ -1,49 +1,44 @@
 #pragma once
 #include <iostream>
-template<typename TNum>
-class CPoint {
-private:
-    TNum x;
-    TNum y;
+
+template <typename TNum>
+class CPoint
+{
+	TNum x, y;
 public:
-    CPoint(TNum a, TNum b) : x(a), y(b) {}
-    CPoint() : x(0), y(0) {}
-
-    TNum get_x() const {
-        return x;
-    }
-
-    TNum get_y() const {
-        return y;
-    }
-
-    void print_on(std::ostream& os) const {
-        os << '(' << x << "; " << y << ')';
-    }
-
-    TNum distance(const CPoint& p) const;
-    CPoint operator+(const CPoint& p) const {
-        return CPoint(this->x + p.x, this->y + p.y);
-    }
-
-    CPoint operator-(const CPoint& p) const {
-        return CPoint(this->x - p.x, this->y - p.y);
-    }
+	CPoint(TNum a, TNum b) :x(a), y(b) {}
+	CPoint() :x(0), y(0) {} // TNum(0) ???
+	TNum get_x() const { return x; }
+	TNum get_y() const { return y; }
+	void print_on(std::ostream& os) const
+	{
+		os << '(' << x << "; " << y << ')';
+	}
+	double distance(const CPoint& p) const;
+	CPoint operator+(const CPoint& p) const
+	{
+		return CPoint(this->x + p.x, this->y + p.y);
+	}
+	CPoint operator-(const CPoint& p) const
+	{
+		return CPoint(this->x - p.x, this->y - p.y);
+	}
 };
 
 template<typename TNum>
-inline TNum CPoint<TNum>::distance(const CPoint& p) const
+inline double CPoint<TNum>::distance(const CPoint& p) const
 {
-    return sqrt(pow(x - p.x, 2) + pow(y - y.x, 2));
+	TNum _y = y - p.y;
+	return sqrt(pow(x - p.x, 2) + (double)_y * _y);
 }
 
 template<typename TNum>
-std::ostream& operator << (std::ostream& os, const CPoint<TNum>& A) {
-    A.print_on(os);
-    return os;
+std::ostream& operator<<(std::ostream& os, const CPoint<TNum>& A)
+{
+	A.print_on(os); return os;
 }
-
 template<typename TNum>
-CPoint<TNum> operator*(const CPoint <TNum>& p, TNum n) {
-    return CPoint<TNum>(p.get_x() * n, p.get_y() * n);
+CPoint<TNum> operator*(const CPoint<TNum>& p, TNum n)
+{
+	return CPoint<TNum>(p.get_x() * n, p.get_y() * n);
 }
